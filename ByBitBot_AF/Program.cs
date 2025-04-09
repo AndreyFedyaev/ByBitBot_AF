7bd6b7c5ea1798406e29079b1c2c87d437b0fe49
Andrey Fedyaev <fedyaev@asc-ural.ru>
2025-04-09 10:04:08 +0500

    Наведение порядка в коде

--- ByBitBot_AF/Program.cs
+++ ByBitBot_AF/Program.cs
@@ -19,38 +19,45 @@ namespace ByBit_TestBot
         static async Task Main(string[] args)
         {
 
-            var bot = new MeanReversionBot(
-                apiKey: "ТВОЙ_API_KEY",
-                apiSecret: "ТВОЙ_SECRET_KEY",
-                symbol: "SOLUSDT"               // или BTCUSDT, ETHUSDT и т.д.
-            );
+            var bot = new MeanReversionBot();
 
             await bot.StartLoopAsync();
         }
 
         public class MeanReversionBot
         {
-            //параметры:
+            //параметры из конфигурации:
+            private bool mode = false;          //режим работы: false - симуляция, true - реальная торговля         
+            private string apiKey = "";         //ключ API KEY из ByBIT
+            private string apiSecret = "";      //ключ API SECRET из ByBIT
+            private string symbol = "";         //торговая пара
+            private int maxBuyAmount = 1;       //максимум средств на покупку
+            private int emaFastLength = 20;           //длина экспоненциальной скользящей средней на короткий период
+            private int emaSlowLength = 50;           //длина экспоненциальной скользящей средней на длинный период
+            private KlineInterval candleInterval = KlineInterval.OneMinute;         //интервал свечей
+            private TimeSpan cycle = TimeSpan.FromSeconds(60);                       //таймер работы
 
-            private string symbol = "";
-
-            
-
+            //свойства
             private readonly BybitRestClient _client;
+            private decimal walletBalance { get; set; }     //общий баланс кошелька
+            private decimal usdtBalance { get; set; }       //количество USDT в кошельке
+            private decimal assetBalance { get; set; }      //количество монет в кошельке
+            private decimal lastPrice { get; set; }         //последняя цена
+            private decimal emaFast { get; set; }           //величина экспоненциальной скользящей средней на короткий период    
+            private decimal emaSlow { get; set; }           //величина экспоненциальной скользящей средней на длинный период
+            private decimal? lastBuyPrice { get; set; } = null;           //цена последней покупки
 
 
-
-            private decimal walletBalance = 20m;
-            private decimal assetAmount = 0m;
-            private decimal? lastBuyPrice = null;
-            private decimal _lastPrice = 0m;
-
-            public MeanReversionBot(string apiKey, string apiSecret)
+            public MeanReversionBot()
             {
                 ReadConfig();
 
-        
-
+                //для симуляции
+                if (!mode)
+                {
+                    usdtBalance = 20m;
+                    assetBalance = 0m;
+                }
 
                 _client = new BybitRestClient(options =>
                 {
@@ -61,59 +68,124 @@ namespace ByBit_TestBot
             private void ReadConfig()
             {
                 //считать из конфигурации потом:
+                mode = false;
+                apiKey = "ТВОЙ_API_KEY";
+                apiSecret = "ТВОЙ_SECRET_KEY";
                 symbol = "SOLUSDT";
+                maxBuyAmount = 10;
+                emaFastLength = 20;
+                emaSlowLength = 50;
+                string _candleInterval = "OneMinute";
+                int _cycle = 10;
+
+
+
+                switch (_candleInterval)
+                {
+                    case "OneMinute":
+                        candleInterval = KlineInterval.OneMinute;
+                        break;
+                    case "ThreeMinutes":
+                        candleInterval = KlineInterval.ThreeMinutes;
+                        break;
+                    case "FiveMinutes":
+                        candleInterval = KlineInterval.FiveMinutes;
+                        break;
+                    case "FifteenMinutes":
+                        candleInterval = KlineInterval.FifteenMinutes;
+                        break;
+                    case "ThirtyMinutes":
+                        candleInterval = KlineInterval.ThirtyMinutes;
+                        break;
+                    case "OneHour":
+                        candleInterval = KlineInterval.OneHour;
+                        break;
+                    case "TwoHours":
+                        candleInterval = KlineInterval.TwoHours;
+                        break;
+                    case "FourHours":
+                        candleInterval = KlineInterval.FourHours;
+                        break;
+                    case "SixHours":
+                        candleInterval = KlineInterval.SixHours;
+                        break;
+                    case "TwelveHours":
+                        candleInterval = KlineInterval.TwelveHours;
+                        break;
+                    case "OneDay":
+                        candleInterval = KlineInterval.OneDay;
+                        break;
+                    case "OneWeek":
+                        candleInterval = KlineInterval.OneWeek;
+                        break;
+                    case "OneMonth":
+                        candleInterval = KlineInterval.OneMonth;
+                        break;
+                    default:
+                        candleInterval = KlineInterval.OneMinute;
+                        break;
+                }
+
+                cycle = TimeSpan.FromSeconds(_cycle);
             }
 
             public async Task StartLoopAsync()
             {
-                Console.WriteLine("Mean Reversion Bot (тестовый режим) запущен...\n");
+                if (mode) Console.WriteLine("Mean Reversion Bot запущен в режиме Online...\n");
+                else Console.WriteLine("Mean Reversion Bot запущен в режиме Симуляции...\n");
 
                 while (true)
                 {
                     try
                     {
+                        //текущая цена
+                        lastPrice = await GetCurrentPriceAsync(symbol);
 
-                        _lastPrice = await GetCurrentPriceAsync();
+                        //получение ema
+                        var (emaFastResult, emaSlowResult) = await GetEmaValuesAsync();
+                        emaFast = emaFastResult;
+                        emaSlow = emaSlowResult;
 
-                        var (ema20, ema50) = await GetEmaValuesAsync();
+                        if(!mode) walletBalance = (assetBalance * lastPrice) + usdtBalance;
 
-                        if (lastBuyPrice == null) Console.WriteLine($"[{DateTime.Now:T}] | Цена: {_lastPrice:F2} > EMA20: {ema20:F2} > EMA50: {ema50:F2} | Баланс =  USDT: {walletBalance:F2}, SOL: {assetAmount:F5}");
-                        else Console.WriteLine($"[{DateTime.Now:T}] | Цена покупки: {lastBuyPrice:F2} < Цена: {_lastPrice:F2} < EMA20: {ema20:F2} < EMA50: {ema50:F2} | Баланс =  USDT: {walletBalance:F2}, SOL: {assetAmount:F5}");
+                        if (lastBuyPrice == null) Console.WriteLine($"[{DateTime.Now:T}] | Цена:{lastPrice:F2} > EMA{emaFastLength}:{emaFast:F2} > EMA{emaSlowLength}:{emaSlow:F2} | Баланс =  USDT:{usdtBalance:F2}, SOL:{assetBalance:F5}, Wallet:{walletBalance:F3}");
+                        else Console.WriteLine($"[{DateTime.Now:T}] | Цена покупки:{lastBuyPrice:F2} < Цена:{lastPrice:F2} < EMA{emaFastLength}:{emaFast:F2} < EMA{emaSlowLength}:{emaSlow:F2} | Баланс =  USDT:{usdtBalance:F2}, SOL:{assetBalance:F5}, Wallet:{walletBalance:F3}");
 
 
                         if (lastBuyPrice == null)
                         {
-                            if (ema20 > ema50 && _lastPrice > ema20)
+                            if (lastPrice > emaFast && emaFast > emaSlow)
                             {
-                                await SimulateBuy();
+                                if(!mode) await SimulateBuy();
                             }
                         }
                         else
                         {
-                            if (_lastPrice > lastBuyPrice && ema20 < ema50 && _lastPrice < ema20)
+                            if (lastBuyPrice < lastPrice && lastPrice < emaFast && emaFast < emaSlow)
                             {
-                                await SimulateSell();
+                                if (!mode) await SimulateSell();
                             }
                         }
 
-                        await Task.Delay(TimeSpan.FromSeconds(10));
-
-
+                        await Task.Delay(cycle);
                     }
                     catch (Exception ex)
                     {
                         Console.WriteLine($"Ошибка: {ex.Message}");
-                        await Task.Delay(TimeSpan.FromSeconds(30));
+                        await Task.Delay(cycle);
                     }
                 }
             }
 
-
-            private async Task<decimal> GetCurrentPriceAsync()
+            /// <summary>
+            /// Получает текущую цену указанной торговой пары
+            /// </summary>
+            /// <param name="readSymbol">торговая пара для получения текущей цены</param>
+            private async Task<decimal> GetCurrentPriceAsync(string readSymbol)
             {
-                var ticker = await _client.V5Api.ExchangeData.GetSpotTickersAsync(_symbol);
+                var ticker = await _client.V5Api.ExchangeData.GetSpotTickersAsync(readSymbol);
                 if (!ticker.Success || ticker.Data == null)
-                    throw new Exception($"Ошибка получения цены: {ticker.Error}");
+                    throw new Exception($"Ошибка получения цены {readSymbol}: {ticker.Error}");
 
                 var item = ticker.Data.List.FirstOrDefault();
                 if (item == null || item.LastPrice == 0)
@@ -122,41 +194,49 @@ namespace ByBit_TestBot
                 return item.LastPrice;
             }
 
-
+            /// <summary>
+            /// Покупка в режиме симуляции
+            /// </summary>
             private Task SimulateBuy()
             {
-                decimal quantityToBuy = Math.Round(walletBalance / _lastPrice, 6);
-                decimal cost = quantityToBuy * _lastPrice;
+                decimal quantityToBuy = Math.Round(usdtBalance / lastPrice, 6);
+                decimal cost = quantityToBuy * lastPrice;
 
-                walletBalance -= cost;
-                assetAmount += quantityToBuy;
-                lastBuyPrice = _lastPrice;
+                usdtBalance -= cost;
+                assetBalance += quantityToBuy;
+                lastBuyPrice = lastPrice;
+                walletBalance = (assetBalance * lastPrice) + usdtBalance;
 
-                Console.WriteLine($"[{DateTime.Now:T}] | Покупка: {quantityToBuy} {_symbol} по {_lastPrice:F2} USDT");
-                Console.WriteLine($"Баланс: {walletBalance:F2} USDT | Активов: {assetAmount} {_symbol}");
+                Console.WriteLine($"[{DateTime.Now:T}] | Покупка {symbol}: {quantityToBuy} по {lastPrice:F2} USDT");
+                Console.WriteLine($"[{DateTime.Now:T}] | Баланс USDT:{usdtBalance:F2} | Активов {symbol}:{assetBalance} | Баланс кошелька: {walletBalance}");
 
                 return Task.CompletedTask;
             }
 
+            /// <summary>
+            /// Продажа в режиме симуляции
+            /// </summary>
             private Task SimulateSell()
             {
-                decimal revenue = assetAmount * _lastPrice;
-                walletBalance += revenue;
+                decimal revenue = assetBalance * lastPrice;
+                usdtBalance += revenue;
+                walletBalance = (assetBalance * lastPrice) + usdtBalance;
 
-                Console.WriteLine($"[{DateTime.Now:T}] | Продажа: {assetAmount} {_symbol} по {_lastPrice:F2} USDT");
-                Console.WriteLine($"Баланс после продажи: {walletBalance:F2} USDT");
+                Console.WriteLine($"[{DateTime.Now:T}] | Продажа {symbol}: {assetBalance} по {lastPrice:F2} USDT");
+                Console.WriteLine($"[{DateTime.Now:T}] | Баланс USDT:{usdtBalance:F2} | Активов {symbol}:{assetBalance} | Баланс кошелька: {walletBalance}");
 
-                assetAmount = 0;
+                assetBalance = 0;
                 lastBuyPrice = null;
 
                 return Task.CompletedTask;
             }
 
-
-
-            public async Task<(decimal ema20, decimal ema50)> GetEmaValuesAsync()
+            /// <summary>
+            /// Рассчитывает значения короткой и длинной экспоненциальной скользящей средней
+            /// </summary>
+            public async Task<(decimal resultEmaFast, decimal resultEmaSlow)> GetEmaValuesAsync()
             {
-                var candles = await GetLastCandlesAsync(); // получи 100+ свечей с ценами
+                var candles = await GetLastCandlesAsync(200);      //получаем информацию о 200 последних свечах
                 var quotes = candles.Select(c => new Quote
                 {
                     Date = c.DT,
@@ -167,25 +247,29 @@ namespace ByBit_TestBot
                     Volume = 0
                 }).ToList();
 
-                var ema20 = quotes.GetEma(20).LastOrDefault()?.Ema;
-                var ema50 = quotes.GetEma(50).LastOrDefault()?.Ema;
+                var emaFastData = quotes.GetEma(emaFastLength).LastOrDefault()?.Ema;
+                var emaSlowData = quotes.GetEma(emaSlowLength).LastOrDefault()?.Ema;
 
-                if (ema20 == null || ema50 == null)
-                    throw new Exception("Недостаточно данных для EMA");
-                return ((decimal)ema20, (decimal)ema50);
+                if (emaFastData == null || emaSlowData == null)
+                    throw new Exception("Недостаточно данных для рассчета значений EMA");
+                return ((decimal)emaFastData, (decimal)emaSlowData);
             }
 
-            public async Task<List<Candle>> GetLastCandlesAsync(int limit = 200)
+            /// <summary>
+            /// Получает список свечей
+            /// </summary>
+            /// <param name="count">количество свечей</param>
+            public async Task<List<Candle>> GetLastCandlesAsync(int count)
             {
                 var result = await _client.V5Api.ExchangeData.GetKlinesAsync(
                     category: Category.Spot,
-                    symbol: _symbol,
-                    interval: KlineInterval.OneMinute,
-                    limit: limit
+                    symbol: symbol,
+                    interval: candleInterval,
+                    limit: count
                 );
 
                 if (!result.Success || result.Data?.List == null)
-                    throw new Exception($"❌ Ошибка получения свечей: {result.Error}");
+                    throw new Exception($"Ошибка получения свечей: {result.Error}");
 
                 return result.Data.List
                     .Select(k => new Candle
@@ -201,12 +285,26 @@ namespace ByBit_TestBot
         }
         public class Candle
         {
+            /// <summary>
+            /// Цена открытия
+            /// </summary>
             public decimal Open { get; set; }
+            /// <summary>
+            /// Максимальная цена
+            /// </summary>
             public decimal High { get; set; }
+            /// <summary>
+            /// Минимальная цена
+            /// </summary>
             public decimal Low { get; set; }
+            /// <summary>
+            /// Цена закрытия
+            /// </summary>
             public decimal Close { get; set; }
+            /// <summary>
+            /// дата/время
+            /// </summary>
             public DateTime DT { get; set; }
-
         }
     }
 }
--- ByBitBot_AF/obj/Debug/net6.0/ByBitBot_AF.AssemblyInfo.cs
+++ ByBitBot_AF/obj/Debug/net6.0/ByBitBot_AF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ByBitBot_AF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cd5665080402336088cf5d57264c6551589b2c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ByBitBot_AF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ByBitBot_AF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
