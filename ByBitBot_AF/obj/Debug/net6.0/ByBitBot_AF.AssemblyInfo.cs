c42ba74043b2b047258cc41f16fd8a074663cbc4
Andrey <Fedyaev-A-A@mail.ru>
2025-04-14 22:31:56 +0500

    ..

--- ByBitBot_AF/obj/Debug/net6.0/ByBitBot_AF.AssemblyInfo.cs
+++ ByBitBot_AF/obj/Debug/net6.0/ByBitBot_AF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ByBitBot_AF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f16f5a6dfd69f12242909961b196d4d3061591d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+741ceb44444d5dbf9bebbda0119526f84a80a381")]
 [assembly: System.Reflection.AssemblyProductAttribute("ByBitBot_AF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ByBitBot_AF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
